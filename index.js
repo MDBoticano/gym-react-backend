db8e598c6a6a5deb6e47cb3fcf61272e0c45740e
Marlo Boticano <m.boticano@gmail.com>
2019-12-16 17:23:15 -0800

    Feature WIP: starting over server in index.js

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
 
-app.use(bodyParser.json())
-
-/* Data */
-exercises = [
-  {
-    id: 1,
-    exerciseName: "push up",
-    region: "arms"
-  }, 
-  {
-    id: 2,
-    exerciseName: "pull up",
-    region: "arms"
-  }, 
-  {
-    id: 3,
-    exerciseName: "sit up",
-    region: "abs"
-  }
-]
-
-/* Routes */
 app.get('/', (request, response) => {
-  response.send('<h1>Gym Backend</h1>');
-})
+  response.send('<h1>Gym React Backend</h1>');
+});
 
-app.get('/api/exercises', (request, response) => {
-  response.json(exercises);
-})
-
-app.get('/api/exercises/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const exercise = exercises.find(exercise => exercise.id === id)
-
-  if (exercise) {
-    response.json(exercise)
-  } else {
-    response.status(404).end()
-  }
-})
-
-
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Gym-React server running on port ${PORT}`);
+});
+
