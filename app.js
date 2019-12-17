0e91addeb6775d63c848bc2b312753fcf68820c1
Marlo Boticano <m.boticano@gmail.com>
2019-12-17 14:04:40 -0800

    WIP: Exercises basic model and API endpoint
    - Exercise is only name at the moment
    - Able to GET and POST exercises

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const config = require('./utilities/config');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+// const cors = require('cors');
 
 const exercisesRouter = require('./controllers/exercises');
 
+const mongoose = require('mongoose');
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+
+// app.use(cors());
+app.use(bodyParser.json());
 app.use('/api/exercises/', exercisesRouter);
 
 module.exports = app;
--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,8 +1,43 @@
 const exercisesRouter = require('express').Router();
 const Exercise = require('../models/exercise');
 
-exercisesRouter.get('/', (request, response) => {
-  response.send('<h1>Exercises</h1>');
+// HTTP GET requests
+exercisesRouter.get('/', async (request, response) => {
+  const exercises = await Exercise.find({});
+  response.json(exercises.map(exercise => exercise.toJSON()));
+});
+
+exercisesRouter.get('/:id', async (request, response) => {
+  try {
+    const exercise = await Exercise.findById(request.params.id);
+    if (exercise) {
+      response.json(exercise.toJSON());
+    } else {
+      response.status(404).end()
+    }
+  }
+  catch (exception) {
+    console.log(exception);
+  }
+});
+
+// HTTP POST requests
+exercisesRouter.post('/', async (request, response) => {
+
+  const body = request.body;
+  console.log('body:', body);
+
+  try {
+    const exercise = new Exercise({
+      name: body.name,
+    });
+  
+    const savedExercise = await exercise.save();
+    response.json(savedExercise.toJSON());
+  }
+  catch (exception) {
+    console.log(exception);
+  }
 });
 
 module.exports = exercisesRouter;
