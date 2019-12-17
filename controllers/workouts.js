30b4389f545523b15c0efe575e73b20e33c7513c
Marlo Boticano <m.boticano@gmail.com>
2019-12-17 15:07:25 -0800

    Added GET & POST for Workouts

---
+++ controllers/workouts.js
@@ -0,0 +1,30 @@
+const workoutsRouter = require('express').Router();
+const Workout = require('../models/workout');
+
+// HTTP GET requests
+workoutsRouter.get('/', async (request,response) => {
+  const workouts = await Workout
+    .find({})
+    .populate('exercises', 'name');
+  response.json(workouts.map(workout => workout.toJSON()));
+});
+
+// HTTP POST requests
+workoutsRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  try {
+    const workout = new Workout({
+      name: body.name,
+      exercises: body.exercises,
+    });
+
+    const savedWorkout = await workout.save();
+    response.json(savedWorkout.toJSON());
+  }
+  catch (exception) {
+    console.log(exception);
+  }
+});
+
+module.exports = workoutsRouter;
