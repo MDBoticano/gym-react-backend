476f9a6f056432c399391aebb7d21a02357be6b0
MDBoticano <MDBoticano@users.noreply.github.com>
2020-01-07 17:00:18 -0800

    Port tweaks

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const config = require('./utilities/config');
 
 const server = http.createServer(app);
 
-const { PORT } = config;
+const { PORT, MONGODB_URI } = config;
 
 server.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`Gym React server running on port ${PORT}`);
+  console.log(`Mongo: ${MONGODB_URI}`);
 });
